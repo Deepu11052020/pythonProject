9cc3e56f0ecc4535199c3c0d0618c3eb86597ef3
Deepu11052020 <deepu.bideveloper@gmail.com>
2024-04-23 16:29:19 +0100

    Initial commit

---
+++ first.py
@@ -0,0 +1,18 @@
+#def print_hi(name):
+#    print(name)
+
+if name == '__main__':
+    os.environ['PYSPARK_PYTHON'] = sys.executable
+    spark = SparkSession.builder.master("local").appName("p2").getOrCreate()
+
+    orderSchema = StructType([
+        StructField("orderid", IntegerType(), nullable=True),
+        StructField("orderdate", StringType(), nullable=True),
+        StructField("custid", IntegerType(), nullable=True),
+        StructField("orderstatus", StringType(), nullable=True)
+    ])
+    #ordersdf = spark.read.option("header","true").schema(orderSchema).csv("C:\\Users\\cheru\\PycharmProjects\\pythonProject1\\orders.csv")
+    ordersdf = spark.read.option("header","true").schema(orderSchema).csv("/user/ec2-user/UKUSMarHDFS/bharathi/orders.csv")
+    print("testing webhook")
+    print("testing 2 webhook")
+    ordersdf.show(5)
